a522b9b1066bda9b4bc6ee6430d1e8f181b8e83a
sesraaksu@outlook.com <sesraaksu@outlook.com>
2023-02-02 10:30:14 +0300

    language

--- src/pages/UserSignupPage.js
+++ src/pages/UserSignupPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { signup } from '../api/apiCalls';
+import { signup, changeLanguage } from '../api/apiCalls';
 import Input from '../components/Input';
+import { withTranslation } from 'react-i18next';
+
 class UserSignupPage extends React.Component {
   state = {
     username: null,
@@ -10,15 +12,17 @@ class UserSignupPage extends React.Component {
     pendingApiCall: false,
     errors: {}
   };
+
   onChange = event => {
+    const { t } = this.props;
     const { name, value } = event.target;
     const errors = { ...this.state.errors };
     errors[name] = undefined;
     if (name === 'password' || name === 'passwordRepeat') {
       if (name === 'password' && value !== this.state.passwordRepeat) {
-        errors.passwordRepeat = 'Password mismatch';
+        errors.passwordRepeat = t('Password mismatch');
       } else if (name === 'passwordRepeat' && value !== this.state.password) {
-        errors.passwordRepeat = 'Password mismatch';
+        errors.passwordRepeat = t('Password mismatch');
       } else {
         errors.passwordRepeat = undefined;
       }
@@ -28,6 +32,7 @@ class UserSignupPage extends React.Component {
       errors
     });
   };
+
   onClickSignup = async event => {
     event.preventDefault();
     const { username, displayName, password } = this.state;
@@ -47,26 +52,50 @@ class UserSignupPage extends React.Component {
     this.setState({ pendingApiCall: false });
   };
 
+  onChangeLanguage = language => {
+    const { i18n } = this.props;
+    i18n.changeLanguage(language);
+    changeLanguage(language);
+  };
+
   render() {
     const { pendingApiCall, errors } = this.state;
     const { username, displayName, password, passwordRepeat } = errors;
-
+    const { t } = this.props;
+    
     return (
       <div className="container">
         <form>
-          <h1 className="text-center">Sign Up</h1>
-          <Input name="username" label="Username" error={username} onChange={this.onChange} />
-          <Input name="displayName" label="Display Name" error={displayName} onChange={this.onChange} />
-          <Input name="password" label="Password" error={password} onChange={this.onChange} type="password" />
-          <Input name="passwordRepeat" label="Password Repeat" error={passwordRepeat} onChange={this.onChange} type="password" />
+          <h1 className="text-center">{t('Sign Up')}</h1>
+          <Input name="username" label={t('Username')} error={username} onChange={this.onChange} />
+          <Input name="displayName" label={t('Display Name')} error={displayName} onChange={this.onChange} />
+          <Input name="password" label={t('Password')} error={password} onChange={this.onChange} type="password" />
+          <Input name="passwordRepeat" label={t('Password Repeat')} error={passwordRepeat} onChange={this.onChange} type="password" />
           <div className="text-center mt-3">
             <button className="btn btn-primary" onClick={this.onClickSignup} disabled={pendingApiCall || passwordRepeat !== undefined}>
-              {pendingApiCall && <span className="spinner-border spinner-border-sm"></span>} Sign Up
+              {pendingApiCall && <span className="spinner-border spinner-border-sm"></span>} {t('Sign Up')}
             </button>
+          </div>
+          <div>
+            <img
+              src="https://www.countryflagicons.com/FLAT/24/TR.png"
+              alt="Turkish Flag"
+              onClick={() => this.onChangeLanguage('tr')}
+              style={{ cursor: 'pointer' }}
+            ></img>
+            <img
+              src="https://www.countryflagicons.com/FLAT/24/US.png"
+              alt="USA Flag"
+              onClick={() => this.onChangeLanguage('en')}
+              style={{ cursor: 'pointer' }}
+            ></img>
           </div>
         </form>
       </div>
     );
   }
 }
-export default UserSignupPage;
+
+const UserSignupPageWithTranslation = withTranslation()(UserSignupPage);
+
+export default UserSignupPageWithTranslation;
