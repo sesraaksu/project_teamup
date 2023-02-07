eda2011f21aa639091efc9dacc61e8f39240a40e
sesraaksu@outlook.com <sesraaksu@outlook.com>
2023-02-07 14:49:34 +0300

    .

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,16 +1,30 @@
 import { createStore } from 'redux';
 import authReducer from './authReducer';
 
-const loggedInState = {
-  isLoggedIn: true,
-  username: 'user1',
-  displayName: 'display1',
-  image: null,
-  password: 'P4ssword'
-};
-
 const configureStore = () => {
-  return createStore(authReducer, loggedInState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+  const teamAuth = localStorage.getItem('team-auth');
+
+  let stateInLocalStorage = {
+    isLoggedIn: false,
+    username: undefined,
+    displayName: undefined,
+    image: undefined,
+    password: undefined
+  };
+
+  if (teamAuth) {
+    try {
+      stateInLocalStorage = JSON.parse(teamAuth);
+    } catch (error) {}
+  }
+
+  const store = createStore(authReducer, stateInLocalStorage, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+  store.subscribe(() => {
+    localStorage.setItem('team-auth', JSON.stringify(store.getState()));
+  });
+
+  return store;
 };
 
 export default configureStore;
