77efe98bd7896c64c6606ff8f99b314c4c5576c5
sesraaksu@outlook.com <sesraaksu@outlook.com>
2023-02-07 19:13:10 +0300

    .

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,67 +1,54 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from '../assets/teamup.png';
 import { Link } from 'react-router-dom';
 import { withTranslation } from 'react-i18next';
 import { connect } from 'react-redux';
 import { logoutSuccess } from '../redux/authActions';
-//import { Authentication } from '../shared/AuthenticationContext';
 
-class TopBar extends Component {
-  //static contextType = Authentication;
+const TopBar = props => {
+  const { t, username, isLoggedIn, onLogoutSuccess } = props;
 
-  onClickLogout = () => {
-    const action = {
-      type: 'logout-success'
-    };
-    this.props.dispatch(action);
-  };
-
-  render() {
-    const { t, username, isLoggedIn, onLogoutSuccess } = this.props;
-
-    let links = (
-      <ul className="navbar-nav ms-auto">
+  let links = (
+    <ul className="navbar-nav ml-auto">
+      <li>
+        <Link className="nav-link" to="/login">
+          {t('Login')}
+        </Link>
+      </li>
+      <li>
+        <Link className="nav-link" to="/signup">
+          {t('Sign Up')}
+        </Link>
+      </li>
+    </ul>
+  );
+  if (isLoggedIn) {
+    links = (
+      <ul className="navbar-nav ml-auto">
         <li>
-          <Link className="nav-link" to="/login">
-            {t('Login')}
+          <Link className="nav-link" to={`/user/${username}`}>
+            {username}
           </Link>
         </li>
-        <li>
-          <Link className="nav-link" to="/signup">
-            {t('Sign Up')}
-          </Link>
+        <li className="nav-link" onClick={onLogoutSuccess} style={{ cursor: 'pointer' }}>
+          {t('Logout')}
         </li>
       </ul>
     );
-
-    if (isLoggedIn) {
-      links = (
-        <ul className="navbar-nav ms-auto">
-          <li>
-            <Link className="nav-link" to={`/user/${username}`}>
-              {username}
-            </Link>
-          </li>
-          <li className="nav-link" onClick={onLogoutSuccess} style={{ cursor: 'pointer' }}>
-            {t('Logout')}
-          </li>
-        </ul>
-      );
-    }
-
-    return (
-      <div className="shadow-sm bg-light mb-2">
-        <nav className="navbar navbar-light container navbar-expand">
-          <Link className="navbar-brand" to="/">
-            <img src={logo} width="80" alt="Teamup Logo" />
-            teamup
-          </Link>
-          {links}
-        </nav>
-      </div>
-    );
   }
-}
+
+  return (
+    <div className="shadow-sm bg-light mb-2">
+      <nav className="navbar navbar-light container navbar-expand">
+        <Link className="navbar-brand" to="/">
+          <img src={logo} width="60" alt="Hoaxify Logo" />
+          Hoaxify
+        </Link>
+        {links}
+      </nav>
+    </div>
+  );
+};
 
 const TopBarWithTranslation = withTranslation()(TopBar);
 
