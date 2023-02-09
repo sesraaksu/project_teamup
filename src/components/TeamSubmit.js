04f63c15b584099f87bbe54fe9568600ea57ef04
sesraaksu@outlook.com <sesraaksu@outlook.com>
2023-02-09 23:07:27 +0300

    fixed

--- src/components/TeamSubmit.js
+++ src/components/TeamSubmit.js
@@ -3,19 +3,29 @@ import { useSelector } from 'react-redux';
 import ProfileImageWithDefault from './ProfileImageWithDefault';
 import { useTranslation } from 'react-i18next';
 import { postTeam } from '../api/apiCalls';
+import { useApiProgress } from '../shared/ApiProgress';
+import ButtonWithProgress from './ButtonWithProgress';
 
 const TeamSubmit = () => {
   const { image } = useSelector(store => ({ image: store.image }));
 
   const [focused, setFocused] = useState(false);
   const [team, setTeam] = useState('');
+  const [errors, setErrors] = useState({});
   const { t } = useTranslation();
 
   useEffect(() => {
     if (!focused) {
       setTeam('');
+      setErrors({});
     }
   }, [focused]);
+
+  useEffect(() => {
+    setErrors({});
+  }, [team]);
+
+  const pendingApiCall = useApiProgress('post', '/api/1.0/teames');
 
   const onClickTeamup = async () => {
     const body = {
@@ -24,26 +34,41 @@ const TeamSubmit = () => {
 
     try {
       await postTeam(body);
-    } catch (error) {}
+      setFocused(false);
+    } catch (error) {
+      if (error.response.data.validationErrors) {
+        setErrors(error.response.data.validationErrors);
+      }
+    }
   };
+
+  let textAreaClass = 'form-control';
+  if (errors.content) {
+    textAreaClass += ' is-invalid';
+  }
 
   return (
     <div className="card p-1 flex-row">
       <ProfileImageWithDefault image={image} width="32" height="32" className="rounded-circle me-1" />
       <div className="flex-fill">
       <textarea
-          className="form-control"
+          className={textAreaClass}
           rows={focused ? '3' : '1'}
           onFocus={() => setFocused(true)}
           onChange={event => setTeam(event.target.value)}
           value={team}
         />
+        <div className="invalid-feedback">{errors.content}</div>
         {focused && (
           <div className="text-end mt-1">
-            <button className="btn btn-primary mt-1" onClick={onClickTeamup}>
-              Teamup
-            </button>
-            <button className="btn btn-light d-inline-flex ms-1 mt-1" onClick={() => setFocused(false)}>
+            <ButtonWithProgress
+              className="btn btn-primary mt-1"
+              onClick={onClickTeamup}
+              text="Teamup"
+              pendingApiCall={pendingApiCall}
+              disabled={pendingApiCall}
+            />
+            <button className="btn btn-light d-inline-flex ms-1 mt-1" onClick={() => setFocused(false)} disabled={pendingApiCall}>
               <i className="material-icons">close</i>
               {t('Cancel')}
             </button>
