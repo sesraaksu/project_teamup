62d6639583f5bd6b0ceece264758db1853c67d94
sesraaksu@outlook.com <sesraaksu@outlook.com>
2023-02-09 22:30:00 +0300

    share

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -32,3 +32,7 @@ export const getUser = username => {
 export const updateUser = (username, body) => {
   return axios.put(`/api/1.0/users/${username}`, body);
 };
+
+export const postTeam = team => {
+  return axios.post('/api/1.0/teames', team);
+};
--- src/components/TeamSubmit.js
+++ src/components/TeamSubmit.js
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import ProfileImageWithDefault from './ProfileImageWithDefault';
+import { useTranslation } from 'react-i18next';
+import { postTeam } from '../api/apiCalls';
 
 const TeamSubmit = () => {
   const { image } = useSelector(store => ({ image: store.image }));
+
+  const [focused, setFocused] = useState(false);
+  const [team, setTeam] = useState('');
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    if (!focused) {
+      setTeam('');
+    }
+  }, [focused]);
+
+  const onClickTeamup = async () => {
+    const body = {
+      content: team
+    };
+
+    try {
+      await postTeam(body);
+    } catch (error) {}
+  };
+
   return (
     <div className="card p-1 flex-row">
       <ProfileImageWithDefault image={image} width="32" height="32" className="rounded-circle me-1" />
       <div className="flex-fill">
-        <textarea className="form-control" />
-        <div className="text-end mt-1">
-          <button className="btn btn-primary">Teamup</button>
-        </div>
+      <textarea
+          className="form-control"
+          rows={focused ? '3' : '1'}
+          onFocus={() => setFocused(true)}
+          onChange={event => setTeam(event.target.value)}
+          value={team}
+        />
+        {focused && (
+          <div className="text-end mt-1">
+            <button className="btn btn-primary mt-1" onClick={onClickTeamup}>
+              Teamup
+            </button>
+            <button className="btn btn-light d-inline-flex ms-1 mt-1" onClick={() => setFocused(false)}>
+              <i className="material-icons">close</i>
+              {t('Cancel')}
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
