5c2564d1548751285690fcdc3b92bb35d030ec91
sesraaksu@outlook.com <sesraaksu@outlook.com>
2023-02-16 18:43:49 +0300

    .

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,21 +1,16 @@
 import axios from 'axios';
-
 export const signup = body => {
   return axios.post('/api/1.0/users', body);
 };
-
 export const login = creds => {
   return axios.post('/api/1.0/auth', {}, { auth: creds });
 };
-
 export const changeLanguage = language => {
   axios.defaults.headers['accept-language'] = language;
 };
-
 export const getUsers = (page = 0, size = 3) => {
   return axios.get(`/api/1.0/users?page=${page}&size=${size}`);
 };
-
 export const setAuthorizationHeader = ({ username, password, isLoggedIn }) => {
   if (isLoggedIn) {
     const authorizationHeaderValue = `Basic ${btoa(username + ':' + password)}`;
@@ -24,34 +19,29 @@ export const setAuthorizationHeader = ({ username, password, isLoggedIn }) => {
     delete axios.defaults.headers['Authorization'];
   }
 };
-
 export const getUser = username => {
   return axios.get(`/api/1.0/users/${username}`);
 };
-
 export const updateUser = (username, body) => {
   return axios.put(`/api/1.0/users/${username}`, body);
 };
-
 export const postTeam = team => {
   return axios.post('/api/1.0/teames', team);
 };
-
 export const getTeames = (username, page = 0) => {
   const path = username ? `/api/1.0/users/${username}/teames?page=` : '/api/1.0/teames?page=';
   return axios.get(path + page);
 };
-
 export const getOldTeames = (id, username) => {
   const path = username ? `/api/1.0/users/${username}/teames/${id}` : `/api/1.0/teames/${id}`;
   return axios.get(path);
 };
-
 export const getNewTeamCount = (id, username) => {
   const path = username ? `/api/1.0/users/${username}/teames/${id}?count=true` : `/api/1.0/teames/${id}?count=true`;
   return axios.get(path);
 };
 
-export const getNewTeames = id => {
-  return axios.get(`/api/1.0/teames/${id}?direction=after`);
+export const getNewTeames = (id, username) => {
+  const path = username ? `/api/1.0/users/${username}/teames/${id}?direction=after` : `/api/1.0/teames/${id}?direction=after`;
+  return axios.get(path);
 };
--- src/components/TeamFeed.js
+++ src/components/TeamFeed.js
@@ -5,47 +5,40 @@ import TeamView from './TeamView';
 import { useApiProgress } from '../shared/ApiProgress';
 import Spinner from './Spinner';
 import { useParams } from 'react-router-dom';
-
 const TeamFeed = () => {
   const [teamPage, setTeamPage] = useState({ content: [], last: true, number: 0 });
-  
   const [newTeamCount, setNewTeamCount] = useState(0);
   const { t } = useTranslation();
   const { username } = useParams();
-
   const path = username ? `/api/1.0/users/${username}/teames?page=` : '/api/1.0/teames?page=';
-
   const initialTeamLoadProgress = useApiProgress('get', path);
-
   let lastTeamId = 0;
   let firstTeamId = 0;
-
   if (teamPage.content.length > 0) {
     firstTeamId = teamPage.content[0].id;
     const lastTeamIndex = teamPage.content.length - 1;
     lastTeamId = teamPage.content[lastTeamIndex].id;
   }
-
   const oldTeamPath = username ? `/api/1.0/users/${username}/teames/${lastTeamId}` : `/api/1.0/teames/${lastTeamId}`;
   const loadOldTeamesProgress = useApiProgress('get', oldTeamPath, true);
 
-  const loadNewTeamesProgress = useApiProgress('get', `/api/1.0/teames/${firstTeamId}?direction=after`, true);
+  const newTeamPath = username
+    ? `/api/1.0/users/${username}/teames/${firstTeamId}?direction=after`
+    : `/api/1.0/teames/${firstTeamId}?direction=after`;
+
+  const loadNewTeamesProgress = useApiProgress('get', newTeamPath, true);
 
   useEffect(() => {
     const getCount = async () => {
       const response = await getNewTeamCount(firstTeamId, username);
-        setNewTeamCount(response.data.count);
+      setNewTeamCount(response.data.count);
     };
-   
     let looper = setInterval(getCount, 1000);
-
     return function cleanup() {
       clearInterval(looper);
     };
   }, [firstTeamId, username]);
-
-  useEffect(() => { 
-
+  useEffect(() => {
     const loadTeames = async page => {
       try {
         const response = await getTeames(username, page);
@@ -55,10 +48,8 @@ const TeamFeed = () => {
         }));
       } catch (error) {}
     };
-
     loadTeames();
   }, [username]);
-
   const loadOldTeames = async () => {
     const response = await getOldTeames(lastTeamId, username);
     setTeamPage(previousTeamPage => ({
@@ -68,7 +59,7 @@ const TeamFeed = () => {
   };
 
   const loadNewTeames = async () => {
-    const response = await getNewTeames(firstTeamId);
+    const response = await getNewTeames(firstTeamId, username);
     setTeamPage(previousTeamPage => ({
       ...previousTeamPage,
       content: [...response.data, ...previousTeamPage.content]
@@ -78,9 +69,9 @@ const TeamFeed = () => {
 
   const { content, last } = teamPage;
 
-  if (content.length === 0){
+  if (content.length === 0) {
     return <div className="alert alert-secondary text-center">{initialTeamLoadProgress ? <Spinner /> : t('There are no teames')}</div>;
-    }
+  }
 
   return (
     <div>
@@ -90,7 +81,7 @@ const TeamFeed = () => {
           style={{ cursor: loadNewTeamesProgress ? 'not-allowed' : 'pointer' }}
           onClick={loadNewTeamesProgress ? () => {} : loadNewTeames}
         >
-          {loadNewTeamesProgress ? <Spinner /> : t('There are new Teames')}
+          {loadNewTeamesProgress ? <Spinner /> : t('There are new teames')}
         </div>
       )}
       {content.map(team => {
@@ -98,16 +89,14 @@ const TeamFeed = () => {
       })}
       {!last && (
         <div
-        className="alert alert-secondary text-center"
-        style={{ cursor: loadOldTeamesProgress ? 'not-allowed' : 'pointer' }}
-        onClick={loadOldTeamesProgress ? () => {} : loadOldTeames}
-      >
-        {loadOldTeamesProgress ? <Spinner /> : t('Load old teames')}
+          className="alert alert-secondary text-center"
+          style={{ cursor: loadOldTeamesProgress ? 'not-allowed' : 'pointer' }}
+          onClick={loadOldTeamesProgress ? () => {} : loadOldTeames}
+        >
+          {loadOldTeamesProgress ? <Spinner /> : t('Load old teames')}
         </div>
       )}
     </div>
   );
-
 };
-
 export default TeamFeed;
