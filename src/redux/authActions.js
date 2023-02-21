9482d7c9f3f7667b198abea8157ae7337f74ea4f
sesraaksu@outlook.com <sesraaksu@outlook.com>
2023-02-21 10:43:00 +0300

    token

--- src/redux/authActions.js
+++ src/redux/authActions.js
@@ -1,10 +1,17 @@
 import * as ACTIONS from './Constants';
-import { login, signup } from '../api/apiCalls';
+import { login, signup, logout } from '../api/apiCalls';
 
 export const logoutSuccess = () => {
-  return {
-    type: ACTIONS.LOGOUT_SUCCESS
-  };
+  return async function(dispatch){
+    try {
+      await logout();
+    } catch (err){
+
+    }
+    dispatch({
+      type: ACTIONS.LOGOUT_SUCCESS
+    })
+  }
 };
 export const loginSuccess = authState => {
   return {
@@ -27,8 +34,9 @@ export const loginHandler = credentials => {
   return async function(dispatch) {
     const response = await login(credentials);
     const authState = {
-      ...response.data,
-      password: credentials.password
+      ...response.data.user,
+      password: credentials.password,
+      token: response.data.token
     };
     dispatch(loginSuccess(authState));
     return response;
